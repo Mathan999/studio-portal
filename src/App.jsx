43969e08b7e257d311e2ffbc08abae24b4391374
Mathan9 <itsmemathankumar@gmail.com>
2025-04-11 19:33:22 +0530

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
